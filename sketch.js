e7e954d2a6d7c61babdc66a30b6cdbbd692cbb97
hashimhayat <hashimhayat@gmail.com>
2016-12-17 13:38:19 -0500

    updated yPos change

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
-/*By Hashim Hayat*/
-
 var screenW = window.innerWidth;    // Screen Width
 var screenH = window.innerHeight;   // Screen Height
-var heart, buttonPressed = true;
+var heart1,heart2,heart3;
+var imgArr = [];
 var hearts = [];
 
 function preload() {
-  heart = loadImage('heart.png');
+  heart1 = loadImage('images/heart.png');
+  heart2 = loadImage('images/heartL.png');
+  heart3 = loadImage('images/heartR.png');
+  imgArr.push(heart1); imgArr.push(heart2); imgArr.push(heart3);
 }
 
 function setup() {
@@ -37,7 +39,7 @@ function displayText(){
   fill("red");
   textSize(50);
   textFont("Helvetica");
-  var phrase = "Flying Hearts using Perlin Noise";
+  var phrase = "I love you, Aaima.";
   text(phrase,screenW/2-textWidth(phrase)/2,(screenH/2));
   fill("black");
   textSize(12);
@@ -46,28 +48,24 @@ function displayText(){
 }
 
 function Heart(){
-  this.img = heart;
+  this.img = imgArr[0];
   this.xPos = random(0,(screenW-30));
   this.yPos = random((screenH-200),screenH);
   this.noiseLocation = random(0,1000);
   this.offset = random(0.05,0.15);
-  
+  this.pull = random(5,10);
   
   this.display = function(){
-    this.yPos -= 7;
+    this.yPos -= this.pull;
     image(this.img, this.xPos, this.yPos);
   }
   
   this.move = function() {
-    // compute how much we should move
     var xMovement = map( noise(this.noiseLocation), 0, 1, -1, 1 );
-    
-    // update our position
     this.xPos += xMovement;
-    
-    // update our noise offset values
     this.noiseLocation += 0.01;
   }
+  
 }
 
 
